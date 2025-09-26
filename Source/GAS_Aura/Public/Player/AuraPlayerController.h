01930f48bec4e51d5ccb566d3c1b54b791801781
聪头的游戏世界 <1322080797@qq.com>
2025-09-26 17:30:54 +0800

    9.动作输入

---
+++ Source/GAS_Aura/Public/Player/AuraPlayerController.h
@@ -0,0 +1,36 @@
+// Copyright Congtou Tech
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "InputActionValue.h"
+#include "GameFramework/PlayerController.h"
+#include "AuraPlayerController.generated.h"
+
+class UInputMappingContext;
+class UInputAction;
+struct FInputActionValue;
+/**
+ * 
+ */
+UCLASS()
+class GAS_AURA_API AAuraPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+public:
+	AAuraPlayerController();
+
+protected:
+	void BeginPlay() override;
+	void SetupInputComponent() override;
+private:
+
+	UPROPERTY(EditAnywhere, Category="Input")
+	TObjectPtr<UInputMappingContext> AuraContext;
+
+	UPROPERTY(EditAnywhere, Category="Input")
+	TObjectPtr<UInputAction> MoveAction;
+
+	void Move(const FInputActionValue& InputActionValue);
+};
